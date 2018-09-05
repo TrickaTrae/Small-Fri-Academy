928847723e59d409a53c311089f63f18079a3d93
Unknown <soccer4goals@hotmail.com>
2018-09-05 14:41:52 -0600

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../Style.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+
+                <header>
+                    Small Fri Academy
+                </header>
+
+            </div>
+        )
+    }
+}
+export default Home;
