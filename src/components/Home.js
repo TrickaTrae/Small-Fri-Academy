71f721573abf81bf98a26217458b56156498ac8f
Unknown <soccer4goals@hotmail.com>
2018-09-07 12:17:39 -0600

    added about me preview on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from './NavBar'
 import Footer from './Footer'
 import '../Style.css'
@@ -6,6 +7,10 @@ import IMG1 from '../Photos&Fonts/IMG1.jpg'
 import IMG2 from '../Photos&Fonts/IMG2.jpg'
 import IMG3 from '../Photos&Fonts/IMG3.jpg'
 import IMG4 from '../Photos&Fonts/IMG4.jpg'
+import Clock from '../Photos&Fonts/clock.png'
+import Ages from '../Photos&Fonts/Ages.jpg'
+import Sandwich from '../Photos&Fonts/sandwich.png'
+
 
 class Home extends Component {
     render() {
@@ -50,6 +55,44 @@ class Home extends Component {
                     {/* ------End of buttons for my carousel------ */}
                 </div>
                 {/* ----------------------------End of Bootstrap carousel------------------------------ */}
+
+
+                {/* -----------------About us section----------------- */}
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h2 className="about-title">About Us</h2>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 about-column">
+                            <img className="clock" src={Clock} alt="Clock" />
+                            <h4>Hours of Operation</h4><br/>
+                            <p>
+                                Monday-Friday:<br/>
+                                7:00am-6:00pm
+                            </p>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 about-column center-column">
+                            <img className="ages" src={Ages} alt="Ages" />
+                            <h4>Age Care Range</h4><br/>
+                            <p>
+                                We care for children from<br/>
+                                6 weeks to 6 years old.
+                            </p>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 about-column">
+                            <img className="sandwich" src={Sandwich} alt="Sandwich" />
+                            <h4>Meals We Provide</h4><br/>
+                            <p>
+                                We provide breakfast, lunch, and<br/>
+                                snacks through-out the day.
+                            </p>
+                        </div>
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 about-column">
+                            <Link to="/About"><button className="about-button">More Info &#187;</button></Link>
+                        </div>
+                    </div>
+                </div>
+                {/* --------------End of about us section-------------- */}
 
                 <Footer />
 
