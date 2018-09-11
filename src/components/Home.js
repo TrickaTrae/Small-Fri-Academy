d4ce0ea3480e6f82d7547757cc5da5f05430dbf2
Unknown <soccer4goals@hotmail.com>
2018-09-11 15:02:41 -0600

    added a "message from directors" section

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import IMG4 from '../Photos&Fonts/IMG4.jpg'
 import Clock from '../Photos&Fonts/clock.png'
 import Ages from '../Photos&Fonts/Ages.jpg'
 import Sandwich from '../Photos&Fonts/sandwich.png'
+import Directors from '../Photos&Fonts/Directors.jpg'
 
 
 class Home extends Component {
@@ -65,25 +66,25 @@ class Home extends Component {
                         </div>
                         <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 about-column">
                             <img className="clock" src={Clock} alt="Clock" />
-                            <h4>Hours of Operation</h4><br/>
+                            <h4>Hours of Operation</h4><br />
                             <p>
-                                Monday-Friday:<br/>
+                                Monday-Friday:<br />
                                 7:00am-6:00pm
                             </p>
                         </div>
                         <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 about-column center-column">
                             <img className="ages" src={Ages} alt="Ages" />
-                            <h4>Age Care Range</h4><br/>
+                            <h4>Age Care Range</h4><br />
                             <p>
-                                We care for children from<br/>
-                                6 weeks to 6 years old.
+                                We care for children from<br />
+                                12 weeks to 9 years old.
                             </p>
                         </div>
                         <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 about-column">
                             <img className="sandwich" src={Sandwich} alt="Sandwich" />
-                            <h4>Meals We Provide</h4><br/>
+                            <h4>Meals We Provide</h4><br />
                             <p>
-                                We provide breakfast, lunch, and<br/>
+                                We provide breakfast, lunch, and<br />
                                 snacks through-out the day.
                             </p>
                         </div>
@@ -93,6 +94,40 @@ class Home extends Component {
                     </div>
                 </div>
                 {/* --------------End of about us section-------------- */}
+
+
+                {/* ---------------director section----------------- */}
+                <div className="container-fluid myParallax">
+                    <div className="row">
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h2 className="director-title">Message From Management</h2>
+                        </div>
+                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <img className="img-fluid director-photo" src={Directors} alt="Directors" />
+                        </div>
+                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <p className="director-text">
+                                Small Fri Academy has been offering child care for 19 years. Our center Was created to be dedicated to the
+                                mental, social, emotional and physical growth of children. The utmost consideration and preparation is taken
+                                in planning a daily schedule that meets the needs of your child. Our center has a home like atmosphere and 
+                                quality teachers who look forward to spending each day with your child. Our easy access to the I-15 freeway 
+                                makes for a quick commute to work. We would love to give you a tour of our center
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                {/* ------------end of director section-------------- */}
+
+
+                {/* ------------testimonial section------------ */}
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h2 className="testimonial-title">What're Small Fri Parents Saying About Us?</h2>
+                        </div>
+                    </div>
+                </div>
+                {/* -----------end of testimonial section---------- */}
 
                 <Footer />
 
