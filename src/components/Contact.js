146bdfb5dde79484f4b81f3eaf1b351c5825c5d4
Unknown <soccer4goals@hotmail.com>
2018-09-25 15:28:22 -0600

    Working google map in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar'
 import '../Style.css'
+import {
+    Map,
+    InfoWindow,
+    Marker,
+    GoogleApiWrapper
+} from 'google-maps-react';
+
 
 class Contact extends Component {
     render() {
@@ -17,27 +24,56 @@ class Contact extends Component {
                     </div>
                 </div>
 
-                <div className="container">
+                <div className="container-fluid">
                     <div className="row">
                         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             <form action="https://formspree.io/soccer4goals@hotmail.com" method="POST" className="myForm">
                                 <p className="formTitle">Questions/Comments/Concerns</p>
                                 <input type="text" name="name" placeholder="*Name" required minLength="2" maxLength="40" className="myInput" /><br />
                                 <input type="email" name="email" placeholder="*Email" required minLength="6" maxLength="55" className="myInput" /><br />
-                                <input type="text" name="number" placeholder="Number" className="myInput" /><br />
+                                <input type="text" name="number" placeholder="Phone" className="myInput" /><br />
                                 <textarea type="text" name="message" placeholder="*Your Message..." required minLength="20" maxLength="400" className="messageInput"></textarea><br />
-                                <input type="submit" value="Submit" className="form-button"/>
+                                <input type="submit" value="Submit" className="form-button" />
                             </form>
                         </div>
                         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                            hello
+                            <p className="contact-text">
+                                <i className="fa fa-clock-o myIcon"></i>Hours: 7:00AM - 6:00PM<br />
+                                <i className="fa fa-map-marker fa-lg myIcon"></i>131 West Woodrow Street, Salt Lake City<br />
+                                <i className="fa fa-phone-square myIcon"></i>801-266-0177<br />
+                                <i className="fa fa-envelope myIcon"></i>smallfrischools@msn.com<br />
+                                <a href="https://facebook.com" className="contact-link"><i className="fa fa-facebook-square myIcon"></i>Facebook</a>
+                            </p>
                         </div>
                     </div>
                 </div>
 
+                <Map
+                    style={{
+                        width: '90%',
+                        height: '80%',
+                        margin: 'auto',
+                        border: '1px solid black',
+                        boxShadow: '2px 2px 7px 0px black'
+                    }}
+
+                    google={this.props.google}
+                    initialCenter={{
+                        lat: 40.6537629,
+                        lng: -111.8947955
+                    }}
+                    zoom={14}
+                >
+                    <Marker
+                        title={'Small Fri Academy'}
+                        position={{ lat: 40.6537629, lng: -111.8947955 }}
+                    />
+                </Map>
 
             </div>
         )
     }
 }
-export default Contact;
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyCLWgKkRuEsTdaRLs06dC4alvxOOOBPOUY")
+})(Contact)
